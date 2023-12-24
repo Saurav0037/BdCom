3a4cb14bbc419be2d620f40d272cd9520403e1a5
Saurav0037 <saurav.bdcom@gmail.com>
2023-12-24 17:25:58 +0600

    new push

---
+++ inputControl/input.h
@@ -0,0 +1,12 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+
+int try_again(int attempt);
+int input_str(char *message, char str[], int max_size);
+int input_int(char *message, int *data);
+int input_char(char *message, char *data);
+
+
+#endif
